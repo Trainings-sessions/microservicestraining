d507a7be4d1c08fd099af37df4b36524054400bb
Trainings-sessions <115974405+Trainings-sessions@users.noreply.github.com>
2022-10-17 12:17:50 +0530

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,12 @@
+public class Account
+{
+    private Integer accountId;
+    private String accountHolderName;
+    private Double availableBalance;
+    private Double currentBalance;
+    private String accountType;
+
+    //getter and setters
+    // add constructor and toString
+
+}
---
+++ Loan.java
@@ -0,0 +1,11 @@
+public class Loan
+{
+    private String loadRequestId;
+    private String customerId;
+    private String relationManagerid;
+    private Double requestedAmount;
+    private String loanType;
+    private String status;
+
+    // add Constructor, Getter and Setter
+}
